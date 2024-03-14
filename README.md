9ba46d66e0137ee6813d334924d58f3474a893f7
danset-1 <danset-1@student.ltu.se>
2024-03-14 12:57:25 +0100

    update
    removed commented code and code not being used, start of readme

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Simulation environment for demand-supply matching
 
-This repository provides a system for simulating and investigating different behaviors and factors in a demand-supply market. Extension of https://github.com/elliotpk/d0020e, see that repository for more details.
+This repository provides a system for simulating and investigating different behaviors and factors in a demand-supply market. Based of https://github.com/elliotpk/d0020e, see that repository for more details.
 
 ## How to run:
+The code is made in Python 3.12 and before you can run the code you will need to have a few things installed:
+# Things to install and get:
+You will need these packages Pymongo, pyyaml, python flask, networkx, pandas and requests.
+You will also need MongoDB https://www.mongodb.com/docs/manual/installation/ .
+You will also need a google maps API key you can create an account for free and get 300$ to use.
+https://developers.google.com/maps/documentation/javascript/get-api-key
+When you have your API key enter it in Src/static/templates/results and switch API_KEY_HERE with your API key
+![Image Alt text](/images/api.png)
+
+## Class Diagram:
+![Image Alt text](/images/uml.png)
--- Src/GUI.py
+++ Src/GUI.py
@@ -30,6 +30,8 @@ for x in temp:
             sellersInfo.append(y)
         i+=1
 
+#Creates array with buyer and sellers location for shortest path calc
+#ex [[['Stockholm', 'Oslo'], ['Stockholm', 'Berlin']], [['Barcelona', 'Winterthur'], ['Barcelona', 'Berlin']]]
 temp = []
 temp2 = []
 sellerPlaces = []
@@ -40,62 +42,17 @@ c = [sellersInfo[1], sellersInfo[11]]
 temp.append(a)
 temp.append(b)
 temp.append(c)
-
 temp2.append(temp)
-
 sellerPlaces = temp2
 
 toHtml = [sellersInfo[0], sellersInfo[4],sellersInfo[0], sellersInfo[8],sellersInfo[0], sellersInfo[12]]
 
-print(sellersInfo)
-print(sellerPlaces)
-# print(buyer)
-
-
-
-
-
-# y = []
-# Row below is used for testing the code without mongodb
-# each = [["id", "buyer", "score", "eco", "fairness", "buyercity", "land", "warehouse1", "seller", "seller1stad", "land", "warehouse2", "seller2", "seller2stad", "land", "warehouse3"], ["id", "buyer", "score", "eco", "fairness", "buyercity2", "land", "warehouse4", "seller", "seller12stad", "land", "warehouse5", "seller2", "seller22stad", "land", "warehouse6"]]
-# for x in mycol.find():
-#   buyersandseller = []
-#   tempHolder = []
-#   z = x
-#   z = list(x.values())
-#   howMany = (len(z)-8) / 4
-#   next = 9
-#   theBuyer = z[5]
-#   while howMany > 0:
-#       pairPerDeal = []
-#       pairPerDeal.append(theBuyer)
-#       pairPerDeal.append(z[next])
-#       pairPerDeal.append(z[7])
-#       pairPerDeal.append(z[next+2])
-#       tempHolder.append(pairPerDeal)
-#       next = next + 4
-#       howMany = howMany -1
-#   y.append(list(x.values()))
-#   buyersandseller.append(tempHolder)
-#   print(buyersandseller)
-# y2=y
 
 companyNames = ['Company 1','Company 2','Company 3', 'Company 4', 'Company 5', 'Company 6']
 
-# z = []
-# for x in city.find():
-#   z.append(list(x.values()))
-# temp = []
-# for x in z:
-#     temp.append(x[1])
-# z = temp
-    
-####
-
+#Gets the cities that the shortest path goes thru
 allWaypoints = []
 waypoints = ""
-import csv, json
-buyersandseller = [[['Stockholm', 'Oslo'], ['Stockholm', 'Berlin']], [['Barcelona', 'Winterthur'], ['Barcelona', 'Berlin']]]
 for each in sellerPlaces:
     tempWaypoint = []
     for pair in each:
@@ -113,9 +70,7 @@ for each in sellerPlaces:
                         waypoints = waypoints.replace(" ->", ",")
             tempWaypoint.append(waypoints)
     allWaypoints.append(tempWaypoint)
-# allWaypoints = json.dumps(allWaypoints)
-# buyersandseller = json.dumps(buyersandseller)
-print(allWaypoints)
+
 
                 
 ####
@@ -124,42 +79,10 @@ print(allWaypoints)
 def index():
     return render_template('index.html')
 
-# companyNames = ""
-y = ""
 
 @app.route('/result')
 def result():
     return render_template("result.html", names = companyNames, data = buyer, allTheRoutes = allWaypoints, buyerandSellers = toHtml, sellersInfo = sellersInfo)
-
-@app.route('/sortfairness')
-def sortfairness():
-    fair = []
-    i = 0
-    sort = []
-    for x in y:
-        fair.append(int(x[2]))
-    for x in fair:
-        sort.append(x)
-    sort.sort(reverse=True)
-
-    temp = []
-    temp2=[]
-    j = 0
-
-    while(j < len(sort)):
-        i = 0
-        while(i < len(sort)):
-            if(int(sort[j]) == int(fair[i])):
-                temp+=[companyNames[i]]
-                temp2+=[y[i]]
-                i+=1
-            else:
-                i+=1
-        y2 = temp2
-        companyNames2 = temp
-        j+=1
-
-    return render_template("sortfairness.html", names = companyNames2, data = y2, allTheRoutes = allWaypoints, buyerandSellers = buyersandseller)
 
 @app.route('/config')
 def config():
@@ -169,11 +92,4 @@ def config():
 htmlLocation = 'http://127.0.0.1:5000'
 webbrowser.open_new_tab(htmlLocation)
 
-# app.run()
-
-
-
-# Skriv i powershell:
-# DSMSimGrupp92023/.venv/Scripts/Activate.ps1
-# python -m flask --app .\Src\GUI.py run
-# Länk för guide: https://medium.com/@dipan.saha/managing-git-repositories-with-vscode-setting-up-a-virtual-environment-62980b9e8106
+app.run()
